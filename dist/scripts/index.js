0b9778e796845efaecf8853f8437f96406509696
mattbristowhub <mattyb006@googlemail.com>
2021-04-05 22:08:08 +0100

    onChange logic, sass files

--- dist/scripts/index.js
+++ dist/scripts/index.js
@@ -20,27 +20,38 @@ window.onload = async () => {
     }
 };
 
-// Display player stats template
+// Display different player stats template
 playerSelect = async (elem) => {
     let temp = document.getElementById('profileTemplate').content;
     let copyHTML = document.importNode(temp, true);
 
-    let image = copyHTML.getElementById('playerImg');
-    image.src = `./assets/p${elem.value}.png`;
-    image.alt = elem.options[elem.options.selectedIndex].text;
+    if (elem.value === 'Select a player...') {
+        // Remove all template elements
+        removeElements();
+        document.getElementsByClassName('card')[0].style.background = '#fff';
+    } else if (document.getElementsByClassName('card')[0].getElementsByTagName('img').length > 0) {
+        // Have an exisiting player template, remove then add new player
+        removeElements();
 
-    const data = await getData();
-    // Find array instance of player
-    const i = data.players.map(x => x.player.id).indexOf(Number(elem.value));
-    copyHTML.getElementById('name').textContent = `${data.players[i].player.name.first} ${data.players[i].player.name.last}`;
-    copyHTML.getElementById('position').textContent = `${data.players[i].player.info.positionInfo}`;
-    copyHTML.getElementById('appearances').textContent = `Appearances: ${data.players[i].stats.find(({ name }) => name === 'appearances').value}`;
-    copyHTML.getElementById('goals').textContent = `Goals: ${data.players[i].stats.find(({ name }) => name === 'goals').value}`;
-    copyHTML.getElementById('assists').textContent = `Assists: ${data.players[i].stats.find(({ name }) => name === 'goal_assist').value}`;
-    copyHTML.getElementById('gpm').textContent = `Goals per match: ${data.players[i].stats.find(({ name }) => name === 'goals').value / data.players[i].stats.find(({ name }) => name === 'appearances').value}`;
-    copyHTML.getElementById('ppm').textContent = `Passes per minute: ${(data.players[i].stats.find(({ name }) => name === 'fwd_pass').value + data.players[i].stats.find(({ name }) => name === 'backward_pass').value) / data.players[i].stats.find(({ name }) => name === 'mins_played').value}`;
+        let image = copyHTML.getElementById('playerImg');
+        image.src = `./assets/p${elem.value}.png`;
+        image.alt = elem.options[elem.options.selectedIndex].text;
 
-    document.getElementsByClassName('card')[0].appendChild(copyHTML);
+        const data = await getData();
+        appendPlayer(elem, data, copyHTML);
+        document.getElementsByClassName('card')[0].style.background = 'linear-gradient(180deg, #fff 50%, #e66465 50%)';
+        document.getElementsByClassName('card')[0].appendChild(copyHTML);      
+    } else {
+        // Find array instance of player then set their player/stat values
+        let image = copyHTML.getElementById('playerImg');
+        image.src = `./assets/p${elem.value}.png`;
+        image.alt = elem.options[elem.options.selectedIndex].text;
+
+        const data = await getData();
+        appendPlayer(elem, data, copyHTML);
+        document.getElementsByClassName('card')[0].style.background = 'linear-gradient(180deg, #fff 50%, #e66465 50%)';
+        document.getElementsByClassName('card')[0].appendChild(copyHTML);
+    }
 };
 
 // Fetch call to return player-stats 
@@ -60,3 +71,25 @@ getData = () => {
         });
     });
 };
+
+removeElements = () => {
+    document.getElementById('playerImg').remove();
+    document.getElementById('name').remove();
+    document.getElementById('position').remove();
+    document.getElementById('appearances').remove();
+    document.getElementById('goals').remove();
+    document.getElementById('assists').remove();
+    document.getElementById('gpm').remove();
+    document.getElementById('ppm').remove();
+}
+
+appendPlayer = (elem, data, copyHTML) => {
+    const i = data.players.map(x => x.player.id).indexOf(Number(elem.value));
+    copyHTML.getElementById('name').textContent = `${data.players[i].player.name.first} ${data.players[i].player.name.last}`;
+    copyHTML.getElementById('position').textContent = `${data.players[i].player.info.positionInfo}`;
+    copyHTML.getElementById('appearances').textContent = `Appearances: ${data.players[i].stats.find(({ name }) => name === 'appearances').value}`;
+    copyHTML.getElementById('goals').textContent = `Goals: ${data.players[i].stats.find(({ name }) => name === 'goals').value}`;
+    copyHTML.getElementById('assists').textContent = `Assists: ${data.players[i].stats.find(({ name }) => name == 'goal_assist') ? data.players[i].stats.find(({ name }) => name == 'goal_assist').value : 0}`;
+    copyHTML.getElementById('gpm').textContent = `Goals per match: ${Number(data.players[i].stats.find(({ name }) => name === 'goals').value / data.players[i].stats.find(({ name }) => name === 'appearances').value).toFixed(2)}`;
+    copyHTML.getElementById('ppm').textContent = `Passes per minute: ${Number((data.players[i].stats.find(({ name }) => name === 'fwd_pass').value + data.players[i].stats.find(({ name }) => name === 'backward_pass').value) / data.players[i].stats.find(({ name }) => name === 'mins_played').value).toFixed(2)}`;
+}
